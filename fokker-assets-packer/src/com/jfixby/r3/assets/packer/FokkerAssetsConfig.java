0ec5ff45552cbfe275cf4f25e757c702016a5288
J Fixby <github@jfixby.com>
2017-08-08 20:35:42 +0200

    cfg

--- fokker-assets-packer/src/com/jfixby/r3/assets/packer/FokkerAssetsConfig.java
+++ fokker-assets-packer/src/com/jfixby/r3/assets/packer/FokkerAssetsConfig.java
@@ -3,6 +3,6 @@ package com.jfixby.r3.assets.packer;
 
 public class FokkerAssetsConfig {
 
-	public static final String FOKKER_LOCAL_ASSETS_HOME = "D:\\[DEV]\\[GIT-3]\\Fokker\\fokker-assets\\assets";
+	public static final String FOKKER_LOCAL_ASSETS_HOME = "D:\\[DEV]\\[GIT-3]\\FokkerAssets\\r3-fokker-assets\\assets";
 
 }
